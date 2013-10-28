103e337828abbee9ff3c0ee7e48984fd1bd9c57f
Kari <kthelgason@gmail.com>
2013-10-28 15:23:32 +0000

    added maven shit and proper dir structure

---
+++ src/main/java/is/0xabad1deaf/helloWorld/App.java
@@ -0,0 +1,13 @@
+package is.0xabad1deaf.helloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
